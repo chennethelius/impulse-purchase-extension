4e2ca324278d6122625c7cfab2832d04db9633b5
Jonathan Cheng <chengj795@gmail.com>
2025-10-25 04:39:34 +0000

    Apply Prettier formatting across repository

--- extension/background.js
+++ extension/background.js
@@ -1,18 +1,18 @@
 // background.js
 
 chrome.runtime.onInstalled.addListener(() => {
-  console.log("Impulse Blocker installed!");
+  console.log('Impulse Blocker installed!');
 });
 
 // Listen for messages from content or overlay scripts
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.type === "requestUnlock") {
+  if (message.type === 'requestUnlock') {
     // Record that this tab or domain was unlocked
     chrome.storage.local.set({ [message.domain]: true });
     sendResponse({ success: true });
   }
 
-  if (message.type === "checkUnlock") {
+  if (message.type === 'checkUnlock') {
     chrome.storage.local.get(message.domain, (result) => {
       sendResponse({ unlocked: !!result[message.domain] });
     });
